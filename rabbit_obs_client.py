f66752b8206e3884b268d658c2f9cfa52227f7d4
Thomas Renninger <trenn@suse.com>
2020-03-26 19:12:33 +0100

    Initial commit

---
+++ rabbit_obs_client.py
@@ -0,0 +1,242 @@
+#!/usr/bin/python3
+import argparse
+import configparser
+import json
+import logging
+import os
+import pwd
+from subprocess import Popen, STDOUT, run, TimeoutExpired
+import sys
+from datetime import datetime
+
+import pika
+
+# https://openbuildservice.org/help/manuals/obs-admin-guide/obs.cha.administration.html#_rabbit_mq_configuration
+# https://rabbit.opensuse.org
+
+LOG_DIR = "/var/log/rabbit_obs/"
+DOWNLOAD_DIR = "/run/rabbit_obs"
+INST_DIR = "/usr/share/rabbit_obs_client"
+DFLT_TRIGGER = "/bin/true"
+
+
+class OBSConnect(object):
+
+    def __init__(self, bs="obs"):
+        self.pkg_list = []
+        self.pkg_dflts = {'project': '', 'package': '', 'repo': '',
+                          'user': '', 'buildarch': '', 'pkg_arch': '', 'trigger_cmd': DFLT_TRIGGER}
+        self.uids = {}
+        self.gids = {}
+
+        if bs == "obs":
+            self.rabbiturl = "amqps://opensuse:opensuse@rabbit.opensuse.org"
+            self.api = "https://api.opensuse.org"
+            self.rpm_url = "https://build.opensuse.org/package/binary/download"
+            self.amqp_prefix = "opensuse.obs"
+            self.conf_file = "/etc/rabbit_obs.conf"
+        elif bs == "ibs":
+            # ToDo: This is untested
+            self.rabbiturl = "amqps://suse:suse@rabbit.suse.de"
+            self.api = "https://api.suse.de"
+            self.rpm_url = "https://build.suse.de/package/binary/download"
+            self.amqp_prefix = "suse.ibs"
+            self.conf_file = "/etc/rabbit_ibs.conf"
+        else:
+            raise Exception("Fatal %s server type does not exist" % bs)
+
+        self.listen_key = self.amqp_prefix + ".package.build_success"
+        self._read_config(self.conf_file)
+
+    def _read_config(self, file: str) -> None:
+        output = ""
+        config = configparser.ConfigParser()
+        config.read(file)
+        for key in config.sections():
+            new_pkg = {}
+            for conf in self.pkg_dflts.keys():
+                new_pkg[conf] = (config[key].get(conf) or self.pkg_dflts[conf])
+                if not new_pkg[conf]:
+                    raise Exception("Package monitor section [%s] does not specify: [%s]" % (key, conf))
+                if conf == "user":
+                    t_pwd = pwd.getpwnam(new_pkg[conf])
+                    self.uids[new_pkg[conf]] = t_pwd.pw_uid
+                    self.gids[new_pkg[conf]] = t_pwd.pw_gid
+            self.pkg_list.append(new_pkg)
+            output += '%s\t%s\t%s\n' % (new_pkg['package'], new_pkg['project'], new_pkg['repo'])
+        logging.info("Found %s package(s) to listen for:\n%s" % (len(self.pkg_list), output))
+        logging.info(self.pkg_list)
+
+    def get_rpm(self, pkg_hit: dict):
+        """ We received an event with a package hit.
+            Now retrieve the built rpm from the server"""
+
+        t_dir = DOWNLOAD_DIR + '/' + pkg_hit['project'] + \
+                    '_' + pkg_hit['package'] + '_' + pkg_hit['repo'] + '_' + \
+                    str(datetime.utcnow().strftime('%d_%m_%Y_%H_%M_%S'))
+        osc_cmd = 'sudo -u {osc_user} /usr/bin/osc -A {osc_api}  getbinaries -d {download_dir} {project} {package} {repo} {buildarch}'.format(
+            osc_user=pkg_hit["user"],
+            osc_api=self.api,
+            download_dir=t_dir,
+            project=pkg_hit["project"],
+            package=pkg_hit["package"],
+            repo=pkg_hit["repo"],
+            buildarch=pkg_hit["buildarch"])
+        try:
+            logging.info("Making dir: [%s]" % t_dir)
+            if not os.path.isdir(t_dir):
+                logging.warning("Calling script: %s", osc_cmd)
+                os.makedirs(t_dir)
+            if not os.path.isdir(t_dir):
+                logging.error("Failed to make dir: %s", t_dir)
+                return
+            os.chown(t_dir, self.uids[pkg_hit["user"]], self.gids[pkg_hit["user"]])
+            run(osc_cmd.split(), timeout=20, check=True)
+        #    except subprocess.CalledProcessError as E:
+        #        logging.error("Calling script: %s failed\n%s", OSC_CMD, E.output)
+        except TimeoutExpired as E:
+            logging.error("Calling script timed out: %s failed\n%s", osc_cmd, E.output)
+            return None
+        except Exception as E:
+            logging.error("Calling download script script failed\n:%s\n%s", osc_cmd, E)
+            return None
+        return t_dir
+
+    def update_pkg(self, t_dir):
+
+#        exec_cmd = ['zypper', 'update', '--no-confirm', '--allow-vendor-change', '-r', \
+#                'home_trenn_cobbler_test_build cobbler']
+        exec_cmd = ['rpm', '-Uvh', t_dir + '/*.rpm']
+        logging.info("Calling script: %s", " ".join(exec_cmd))
+        try:
+            run(exec_cmd, timeout=20, check=True)
+        except Exception as E:
+            logging.error("Calling download script script failed\n:%s\n%s", exec_cmd, E)
+            return None
+
+    def trigger_cmd(self, pkg):
+
+        log_file = LOG_DIR + pkg['project'] + \
+                    '_' + pkg['package'] + '_' + pkg['repo'] + '_' + \
+                    str(datetime.utcnow().strftime('%d_%m_%Y_%H_%M_%S')) + '.log'
+        with open(log_file, "wb") as out:
+            exec_cmd = [pkg['trigger_cmd'], pkg['project'], pkg['package'], pkg['repo']]
+            logging.info("Calling script: %s", " ".join(exec_cmd))
+            logging.info("Logging script to : %s", log_file)
+            child = Popen(exec_cmd, shell=True,
+                          stdout=out, stderr=out, bufsize=-1)
+        return True
+
+    def rabbit_cb(self, ch, method, properties, body):
+
+        if not method or not method.routing_key:
+            return
+
+        logging.debug(" [x] %r:%r" % (method.routing_key, body))
+
+        try:
+            decoded_body = json.loads(body.decode('utf-8'))
+            project = decoded_body.get("project")
+            package = decoded_body.get("package")
+            arch = decoded_body.get("arch")
+            repo = decoded_body.get("repository")
+            #            versrel = decoded_body.get("versrel")
+            #            reason = decoded_body.get("reason")
+            #            bcnt = decoded_body.get("bcnt")
+        except ValueError as e:
+            logging.error(e)
+            logging.error(" [x] %r:%r" % (method.routing_key, body))
+            return
+
+        if not project or not package or not arch or not repo:
+            logging.warning("Suspicious build success even missing info %s" % body)
+            return
+
+        pkg_hit = None
+        for i, d in enumerate(self.pkg_list):
+            if d["project"] == project and \
+                    d["package"] == package and \
+                    d["repo"] == repo and \
+                    d["buildarch"] == arch:
+                pkg_hit = d
+                break
+
+        if pkg_hit == None:
+            return
+
+        logging.info(" [x] %r:%r" % (method.routing_key, body))
+        t_dir = self.get_rpm(pkg_hit)
+        if not t_dir:
+            logging.error("Could not download rpm(s)")
+            return
+
+        if self.update_pkg(t_dir) is None:
+            return
+
+        if self.trigger_cmd(pkg_hit) is False:
+            return
+
+    def connect(self):
+        # connection = pika.BlockingConnection(pika.URLParameters("amqps://suse:suse@rabbit.suse.de"))
+        while True:
+        # See https://pypi.org/project/pika/
+        # Threading multiple connections (obs and ibs) is hard
+        # Currently only one connecion is supported
+            try:
+                connection = pika.BlockingConnection(pika.URLParameters(
+                    "amqps://opensuse:opensuse@rabbit.opensuse.org"))
+                channel = connection.channel()
+
+                channel.exchange_declare(exchange='pubsub', exchange_type='topic',
+                                     passive=True, durable=True)
+
+                result = channel.queue_declare("", exclusive=True)
+                queue_name = result.method.queue
+
+                channel.queue_bind(exchange='pubsub', queue=queue_name,
+                               routing_key=self.listen_key)
+
+                print(' [*] Waiting for logs. To exit press CTRL+C')
+
+                channel.basic_consume(queue_name,
+                                  self.rabbit_cb,
+                                  auto_ack=True)
+
+                channel.start_consuming()
+            except pika.exceptions.ConnectionClosedByBroker:
+                break
+            # Don't recover on channel errors
+            except pika.exceptions.AMQPChannelError:
+                break
+            # Recover on all other connection errors
+            except pika.exceptions.AMQPConnectionError:
+                continue
+
+def main():
+
+    # Logging
+    logging.basicConfig(filename=LOG_DIR + 'rabbit.log',
+                        format='%(asctime)s %(message)s',
+                        level=logging.INFO)
+    logging.getLogger().addHandler(logging.StreamHandler())
+    logging.getLogger().addHandler(logging.StreamHandler(sys.stdout))
+
+    # Argument parsing
+    parser = argparse.ArgumentParser()
+    parser.parse_args()
+    parser.add_argument("--server", help="OpenSUSE Build Server", choices=["obs", "ibs"], default="obs", required=False)
+    args = parser.parse_args()
+
+    # Start server
+    server = OBSConnect(args.server)
+#    server.connect()
+#    pkg = {'project': 'home:trenn:cobbler_mainline_sle15_sp1', 'package': 'cobbler',
+#           'repo': 'SUSE_SLE-15-SP1_GA_standard', 'user': 'trenn', 'buildarch': 'x86_64', 'pkg_arch': 'noarch',
+#           'trigger_cmd': 'sleep 10'}
+    t_dir = server.get_rpm(server.pkg_list[0])
+#    if t_dir:
+#    server.update_pkg(t_dir)
+#    server.trigger_cmd(pkg)
+
+if __name__ == '__main__':
+    main()
---
+++ rabbit_obs_client.spec
@@ -0,0 +1,71 @@
+#
+# spec file for package rabbit_obs_client
+#
+# Copyright (c) 2019 SUSE LINUX GmbH, Nuernberg, Germany.
+#
+# All modifications and additions to the file contributed by third parties
+# remain the property of their copyright owners, unless otherwise agreed
+# upon. The license for this file, and modifications and additions to the
+# file, is the same license as for the pristine package itself (unless the
+# license for the pristine package is not an Open Source License, in which
+# case the license is the MIT License). An "Open Source License" is a
+# license that conforms to the Open Source Definition (Version 1.9)
+# published by the Open Source Initiative.
+
+# Please submit bugfixes or comments via https://bugs.opensuse.org/
+#
+
+
+Name:           rabbit_obs_client
+Version:        1.0
+Release:        0
+Summary:        Download, install and test packages on OpenSUSE Build Service build_success events
+License:        GPL-3.0
+Group:          System/Monitoring
+Url:            https://github.com/rabbit_obs_client/rabbit_obs_client
+Source:         %{name}-%{version}.tar.xz
+BuildRequires:  pkgconfig(systemd)
+BuildRequires:  python-rpm-macros
+%python_subpackages
+
+%description
+Use this package for automated tests, which are triggered once the OBS server
+has performed a successful build.
+OpenSUSE Build Service rabbit client is a systemd service written in python.
+It listens to rabbitmq build_success events from an OBS server.
+If an event is captured that matches a package in the config file, the newly
+built package rpm is retrieved from the OBS server, the local system is updated
+with the package and a specified command which may start tests is executed and logged.
+
+%prep
+%autosetup
+
+%build
+
+
+%install
+install -D -m 0755 rabbit_obs_client.py  %{buildroot}/usr/share/%{name}/rabbit_obs_client
+install -D -m 0644 rabbit_obs_client.service %{buildroot}%{_unitdir}/rabbit_obs_client.service
+install -D -m 0644 rabbit_obs.conf %{buildroot}/%{_sysconfdir}/rabbit_obs.conf
+ln -sf %{_sbindir}/service %{buildroot}%{_sbindir}/rcrabbit_obs_client
+
+
+%pre
+%service_add_pre %{name}.service
+
+%post
+%service_add_post %{name}.service
+
+%preun
+%service_del_preun %{name}.service
+
+%postun
+%service_del_postun %{name}.service
+
+%files
+%config %{_sysconfdir}/rabbit_obs.conf
+%{_unitdir}/rabbit_obs_client.service
+%{_sbindir}/rcrabbit_obs_client
+%ghost /var/run/rabbit_obs_client
+
+%changelog
